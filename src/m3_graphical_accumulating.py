f38127f3b4442a8aac397ae4b1bd9b8cb7da9b73
fletchjm <fletchjm@rose-hulman.edu>
2019-03-11 11:39:51 -0400

    DONE

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Micah Fletcher.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -96,7 +96,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -109,6 +109,23 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+
+
+    x = point.x
+    y = point.y
+
+
+    for k in range(n):
+        start = rg.Point(x , y)
+        end = rg.Point(x + length, y)
+        line = rg.Line(start, end)
+
+        line.attach_to(window)
+
+        y = y + 30
+
+    window.render()
+
 
 
 def run_test_draw_lines():
@@ -175,6 +192,22 @@ def draw_lines(n, point, window):
     ###########################################################################
     # -------------------------------------------------------------------------
 
+    x = point.x
+    y = point.y
+
+    step = 200/(n-1)
+
+    for k in range(n):
+        start = rg.Point(x, y)
+        y2 = y + step * k
+        end =rg.Point(x + 100, y2 - 100)
+        line = rg.Line(start,end)
+
+        line.attach_to(window)
+
+
+
+    window.render()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
